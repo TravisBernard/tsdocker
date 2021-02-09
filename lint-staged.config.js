07584f6e0f6d7a280d4d65e5915f9ef6be43739e
Travis Bernard <travis.bernard@shipperhq.com>
2021-02-08 18:11:39 -0600

    starts stubbing in testing on every commit (using lint-staged)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 const prettier = "yarn prettier --write";
 const typecheck = () => "yarn tsc --noEmit";
+const test = "yarn run test:fast";
 
 module.exports = {
-    "*.{ts,tsx}": [prettier, typecheck],
-    "*.{graphql,yml,md,scss,js,jsx}": [prettier],
+    "*.{ts,tsx}": [prettier, typecheck, test],
+    "*.{graphql,yml,md,scss,js,jsx,json}": [prettier, test],
 };
